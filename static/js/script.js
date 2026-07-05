055b5026f41ccf5b3a8de5a0eb8fdeb52715df56
Shaun07a <shaunjoseph004@gmail.com>
2026-07-06 00:47:28 +0530

    Day 5: Final UI improvements and deployment preparation

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,31 @@
-console.log("Car Price Prediction Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.querySelector("form");
+    const button = form.querySelector("button[type='submit']");
+
+    form.addEventListener("submit", () => {
+
+        button.disabled = true;
+
+        button.innerHTML = `
+            <span class="spinner-border spinner-border-sm me-2"></span>
+            Predicting...
+        `;
+
+    });
+
+    const inputs = document.querySelectorAll("input[type='number']");
+
+    inputs.forEach(input => {
+
+        input.addEventListener("input", () => {
+
+            if (input.value < 0) {
+                input.value = 0;
+            }
+
+        });
+
+    });
+
+});
